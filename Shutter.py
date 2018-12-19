26ed94973d57a6e482b119b86884dac80d68d857
Holger Wolff <holger.wolff@monash.edu>
2018-12-19 16:22:15 +1100

    add Shutter.py

---
+++ Shutter.py
@@ -0,0 +1,11 @@
+import HBridge
+
+class Shutter():
+    def __init__(self, forward_in, forward_out, reverse_in, reverse_out):
+        self.hbridge = HBridge(forward_in, forward_out, reverse_in, reverse_out)
+    def close(self):
+        self.hbridge.forward()
+    def open(self):
+        self.hbridge.reverse()
+    def stop(self):
+        self.hbridge.off()
