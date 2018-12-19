63c9820bcda046468419e346be0f37b659efa724
Holger Wolff <holger.wolff@monash.edu>
2018-12-19 16:17:03 +1100

    initial commit

---
+++ HBridge.py
@@ -0,0 +1,40 @@
+import piplates.RELAYplate as RELAY
+
+class HBridge():
+    def __init__(self, forward_in, forward_out, reverse_in, reverse_out):
+        self.forward_in = forward_in
+        self.forward_out = forward_out
+        self.reverse_in = reverse_in
+        self.reverse_out = reverse_out
+        self.nevertogether = {
+            self.forward_in : self.reverse_out,
+            self.reverse_out : self.forward_in,
+            self.forward_out : self.reverse_in,
+            self.reverse_in : self.forward_out
+        }
+        self.off()
+    def forward(self):
+        self.__relayOFF(self.reverse_in)
+        self.__relayOFF(self.reverse_out)
+        self.__relayON(self.forward_in)
+        self.__relayON(self.forward_out)
+    def reverse(self):
+        self.__relayOFF(self.forward_in)
+        self.__relayOFF(self.forward_out)
+        self.__relayON(self.reverse_in)
+        self.__relayON(self.reverse_out)
+    def off(self):
+        self.__relayOFF(self.forward_in)
+        self.__relayOFF(self.forward_out)
+        self.__relayOFF(self.reverse_in)
+        self.__relayOFF(self.reverse_out)
+    def brake(self):
+        self.__relayOFF(self.forward_in)
+        self.__relayOFF(self.reverse_in)
+        self.__relayON(self.forward_out)
+        self.__relayON(self.reverse_out)
+    def __relayOFF(self, channel)
+        RELAY.relayOFF(channel[0], channel[1])
+    def __relayON(self, channel)
+        self.__relayOFF(self.nevertogether[channel])
+        RELAY.relayON(channel[0], channel[1])
