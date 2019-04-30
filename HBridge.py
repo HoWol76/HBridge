b67c0ecb7a410f6ea17aa8e5469ccf645c7f5714
Holger Wolff <holger.wolff@monash.edu>
2019-04-30 11:44:04 +1000

    dummy change

--- HBridge.py
+++ HBridge.py
@@ -42,3 +42,5 @@ class HBridge:
     def __relayON(self, channel):
         self.__relayOFF(self.nevertogether[channel])
         RELAY.relayON(channel[0], channel[1])
+
+# Dummy comment to test something on github
