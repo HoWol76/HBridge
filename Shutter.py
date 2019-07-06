309cc8cc561b589413255a4764a22943cb50d2aa
Holger Wolff <holger.wolff@monash.edu>
2019-07-06 14:30:37 +1000

    add ability to control shutters independently

--- Shutter.py
+++ Shutter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from HBridge import HBridge
+import argparse
 
 class Shutter:
     """
@@ -16,29 +17,67 @@ class Shutter:
     def stop(self):
         self.hbridge.off()
 
+Shutters = {
+        'living' : Shutter((0, 1), (0, 2), (0, 3), (0, 4)),
+        'stairs' : Shutter((1, 1), (1, 2), (1, 3), (1, 4)),
+        'master' : Shutter((3, 1), (3, 2), (3, 3), (3, 4)),
+        'sarah'  : Shutter((0, 5), (1, 5), (3, 5), (0, 6))
+        }
+
+
+def func_open(args):
+    for s in args.Shutter:
+        if s in Shutters.keys():
+            if args.verbose:
+                print("Opening Shutter {s}".format(s=s))
+            Shutters[s].open()
+        else:
+            if args.verbose:
+                print("{} not recognised as Shutter".format(s))
+                print("Please select one of {}".format(list(Shutters.keys())))
+
+
+def func_close(args):
+    for s in args.Shutter:
+        if s in Shutters.keys():
+            if args.verbose:
+                print("Closing Shutter {s}".format(s=s))
+            Shutters[s].close()
+        else:
+            if args.verbose:
+                print("{} not recognised as Shutter".format(s))
+                print("Please select one of {}".format(list(Shutters.keys())))
+
+
+def func_stop(args):
+    for s in args.Shutter:
+        if s in Shutters.keys():
+            if args.verbose:
+                print("Stopping Shutter {s}".format(s=s))
+            Shutters[s].stop()
+        else:
+            if args.verbose:
+                print("{} not recognised as Shutter".format(s))
+                print("Please select one of {}".format(list(Shutters.keys())))
+
+
 def main():
-    import optparse
-    parser = optparse.OptionParser("Manual Control of the Shutters")
-    (options, args) = parser.parse_args()
-    living = Shutter((0, 1), (0, 2), (0, 3), (0, 4))
-    stairs = Shutter((1, 1), (1, 2), (1, 3), (1, 4))
-    master = Shutter((3, 1), (3, 2), (3, 3), (3, 4))
-    sarah = Shutter((0, 5), (1, 5), (3, 5), (0, 6))
-    if 'close' in args:
-        living.close()
-        stairs.close()
-        master.close()
-        sarah.close()
-    elif 'open' in args:
-        living.open()
-        stairs.open()
-        master.open()
-        sarah.open()
-    elif 'stop' in args:
-        living.stop()
-        stairs.stop()
-        master.stop()
-        sarah.stop()
+    parser=argparse.ArgumentParser(description="Testing Script for argparse")
+    parser.add_argument('--verbose', '-v', help='Verbose', action='store_true')
+    shutters=argparse.ArgumentParser(add_help=False)
+    shutters.add_argument('Shutter', nargs='*', help='Shutters to operate (default: all)',
+            default=list(Shutters.keys()))
+    action = parser.add_subparsers(help='Action')
+    ac_open = action.add_parser('open', parents=[shutters], help="Open Shutters")
+    ac_close = action.add_parser('close', parents=[shutters], help="Close Shutters")
+    ac_stop = action.add_parser('stop', parents=[shutters], help="Stop Shutters")
+    ac_open.set_defaults(func=func_open)
+    ac_close.set_defaults(func=func_close)
+    ac_stop.set_defaults(func=func_stop)
+    args = parser.parse_args()
+    
+    args.func(args)
+
 
 if __name__ == '__main__':
     main()
